a2e133086eedbceb06f2abf7b9f66d2a3f8a2be8
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-05-30 08:48:57 +0300

    Low level web application(Generate headers and body)
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

---
+++ main.py
@@ -0,0 +1,59 @@
+import socket
+
+URLS = {
+    '/': 'Hello index',
+    '/blog': 'Hello blog'
+}
+
+def parse_request(request):
+    parsed = request.split(' ')
+    method = parsed[0]
+    url = parsed[1]
+    return (method, url)
+
+
+def generate_headers(method, url):
+    if not method == 'GET':
+        return ('HTTP/1.1 405 Method not allowed\n\n', 405)
+
+    if not url in URLS:
+        return ('HTTP/1.1 404 Not found\n\n', 404)
+
+    return ('HTTP/1.1 200 OK\n\n', 200)
+
+
+def generate_content(code, url):
+    if code == 404:
+        return '<h1>403</h1><p>Not found</p>'
+    if code == 405:
+        return '<h1>405</h1><p>Method not ellowed</p>'
+    return '<h1>{}</h1>'.format(URLS[url])
+
+
+def generate_response(request):
+    method, url = parse_request(request)
+    headers, code = generate_headers(method, url)
+    body = generate_content(code, url)
+
+    return (headers + body).encode()
+
+
+def run():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(('127.0.0.1', 8000))
+    server_socket.listen()
+
+    while True:
+        clint_socket, addr = server_socket.accept()
+        request = clint_socket.recv(1024)
+        print(request)
+
+        response = generate_response(request.decode('utf-8'))
+
+        clint_socket.sendall(response)
+        clint_socket.close()
+
+
+if __name__ == '__main__':
+    run()
